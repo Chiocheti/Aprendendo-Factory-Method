260791373ea400dce11721686b2d1819bcdc9cad
Chiocheti <gabrielchiocheti@gmail.com>
2021-09-10 22:08:52 -0300

    Adicionando as Pizzas de São João

---
+++ SjMarguerita.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+public class SjMarguerita extends Pizza{
+    
+    public SjMarguerita(){
+        
+        nome = "Marguerita ao estilo de São João";
+        massa = "Media";
+        molho = "Temperado";
+        temperatura = "280";
+        
+        coberturas.add("Muito Queijo");
+    }
+}
---
+++ SjMussarela.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+public class SjMussarela extends Pizza{
+    
+        public SjMussarela(){
+        
+        nome = "Mussarela ao estilo de São João";
+        massa = "Grossa Crocante";
+        molho = "Russo";
+        temperatura = "300";
+        
+        coberturas.add("Salsichas");
+    }
+}
